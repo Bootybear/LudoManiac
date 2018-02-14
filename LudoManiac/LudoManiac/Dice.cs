a39e3232e83f397ae110981045add83187cd2a28
Bootybear <asbjornwulff@hotmail.com>
2018-02-14 13:42:54 +0100

    Resten

---
+++ LudoManiac/LudoManiac/Dice.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LudoManiac
+{
+    class Dice
+    {
+        //Dice value
+        private Random rng = new Random();
+        private int diceValue;
+
+        //Constructor to throw dice
+        public Dice()
+        {
+            this.diceValue = this.rng.Next(1, 7);
+        }
+
+        //Method to throw dice
+        public int ThrowDice()
+        {
+            this.diceValue = this.rng.Next(1, 7);
+
+            for (int i = 3; i > 0; i--)
+            {
+                Console.Write(" . ");
+                System.Threading.Thread.Sleep(500);
+            }
+
+            return this.diceValue;
+        }
+
+        //Checks dice value
+        public int GetValue()
+        {
+            return this.diceValue;
+        }
+
+    }
+}
---
+++ LudoManiac/LudoManiac/Game.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LudoManiac
+{
+    //Ludo colors
+    public enum GameColor { Gul, Blå, Rød, Grøn };
+    public enum GameState { InPlay, Finished };
+
+    public class Game
+    {
+        public string playerName { get; set; }
+        public string GameColor;
+        public int spillerMaengde;
+        public int i;
+        public string playerOne;
+        public string name;
+
+        private Dice dice = new Dice();
+        //private Player hejsa = new Player();
+        
+        //Starts a new game
+        public Game()
+        {
+            CreatePlayer();
+
+        }
+
+        public void CreatePlayer()
+        {
+
+            List<string> player = new List<string>();
+            Console.WriteLine("Hvor mange spillere? (1-4):");
+            spillerMaengde = Int32.Parse(Console.ReadLine());
+
+            while (spillerMaengde > 0 && spillerMaengde <= 4)
+            {
+                for (int i = 1; i <= spillerMaengde; i++)
+                {
+                    Console.WriteLine("Hvad hedder spiller " + i + "?:");
+                    playerName = Console.ReadLine();
+                    player.Add(playerName);
+                    //Console.ReadLine();
+                }
+
+                switch (spillerMaengde)
+                {
+                    case 1:
+                        Console.WriteLine("Spiller 1's navn er: " + player[0]);
+                        Console.ReadLine();
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Spiller 1's navn er: " + player[0]);
+                        Console.WriteLine("Spiller 2's navn er: " + player[1]);
+                        Console.ReadLine();
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Spiller 1's navn er: " + player[0]);
+                        Console.WriteLine("Spiller 2's navn er: " + player[1]);
+                        Console.WriteLine("Spiller 3's navn er: " + player[2]);
+                        Console.ReadLine();
+                        break;
+                    case 4:
+                        Console.WriteLine("Spiller 1's navn er: " + player[0]);
+                        Console.WriteLine("Spiller 2's navn er: " + player[1]);
+                        Console.WriteLine("Spiller 3's navn er: " + player[2]);
+                        Console.WriteLine("Spiller 4's navn er: " + player[3]);
+                        Console.ReadLine();
+                        break;
+                    default:
+                        Console.WriteLine("Too many players! Pick a number from 1-4");
+                        spillerMaengde = Int32.Parse(Console.ReadLine());
+                        break;
+                }
+                break;
+            }
+        }
+    }
+}
+
